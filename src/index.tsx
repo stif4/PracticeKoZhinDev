2b6f8d5572df9cd9b559ead7884ccfa34b99bbc5
amstry <vanva44@gmail.com>
2024-02-04 02:10:27 +0700

    some tips for improving

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,13 @@ import {Provider} from 'react-redux';
 import Application from './Application';
 import {store} from './store/store';
 
+//1) зарефакторить компоненты, button и прочие сделать как в inputmultiselect, разбить все по папкам.
+//2) зарефакторить api, обработка ошибок все в классы.
+//3) переделать компоненты form, использотвать react hook forms
+//4) адаптировать
+//5) написать тесты
+//6) подебажить запросы и ререндаренги, подумать как можно оптимизировать.
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
